d1fb4b6faaba658d154886dc5ecb76bee7b41fba
P Kalki Venkatesh <49164504+Kalkivenkatesh@users.noreply.github.com>
2020-08-19 21:50:25 +0530

    Create delection node in a linked kist.cpp

---
+++ delection node in a linked kist.cpp
@@ -0,0 +1,11 @@
+//Deletion of linked list
+
+
+
+class Solution {
+public:
+     void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;  
+    }
+};
